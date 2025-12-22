aba3ba23507e558b6fff27b03959e78fd40da604
xchnly <hendryhuang70@gmail.com>
2025-12-22 20:25:08 +0700

    env

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,17 +1,18 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyADzP1OjVoNGTWp0V30hue2B3yh0-fr9Os",
-  authDomain: "asmara-pos.firebaseapp.com",
-  projectId: "asmara-pos",
-  storageBucket: "asmara-pos.firebasestorage.app",
-  messagingSenderId: "1065016328628",
-  appId: "1:1065016328628:web:57418690c4988b4e4672e1"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
-const app = initializeApp(firebaseConfig);
+// 🔒 Cegah initialize berulang (Next.js hot reload)
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
